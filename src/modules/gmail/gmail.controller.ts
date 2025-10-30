bedc15d0051c8a3f4e03fd5299c8d22162a4ed84
jhonny-berdeja <jhonny.berdeja.100@gmail.com>
2025-10-30 16:16:50 -0300

    Adde swagger to send email

--- src/modules/gmail/gmail.controller.ts
+++ src/modules/gmail/gmail.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, applyDecorators } from '@nestjs/common';
 import { GmailDataDto } from './dto/gmail.data.dto';
 import { GmailService } from './gmail.service';
+import { SwaggerGmailDocs } from './docs/gmail.swagger';
 
 @Controller('email')
 export class GmailController {
   constructor(private readonly gmailService: GmailService) {}
 
   @Post('send')
+  @applyDecorators(...SwaggerGmailDocs.sendMessage)
   sendMessaje(@Body() emailDataDto: GmailDataDto) {
     return this.gmailService.sendMessage(emailDataDto);
   }
